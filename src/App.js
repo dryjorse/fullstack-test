a3788fbf813ffc59e2d1063dc362b174ff47f946
dryjorse <kozoevabdulazim@gmail.com>
2022-12-25 23:34:33 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({})
+
+  useEffect(() => {
+    fetch('/posts').then(resp => resp.json())
+    .then(data => setState(data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>{state.name}</span>
+          <span>{state.count}</span>
         </p>
         <a
           className="App-link"
